59125d5db3beab63b2199460a2e2e2e9f92f78cb
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-12-07 02:10:26 -0600

    second challenge of adventjs

--- 02/a.ts
+++ 02/a.ts
@@ -4,8 +4,37 @@ const holidays = ['01/06', '04/01', '12/25'] // format MM/DD
 // 01/06 is January 6, Thursday. Count.
 // 04/01 is April 1, Friday. Count.
 // 12/25 is December 25, Sunday. Do not count.
-countHours(year, holidays) // 2 days -> 4 extra hours in the year
 
-function countHours(year, holidays) {
-
+function countHours(year: number, holidays: string[]) {
+    let totalHours = 0
+    for (let i = 0; i < holidays.length; i++) {
+        const date = new Date(`${year}/${holidays[i]}`)
+        const day = date.getDay()
+        switch (day) {
+            case 0:
+                totalHours += 0
+                break
+            case 1:
+                totalHours += 2
+                break
+            case 2:
+                totalHours += 2
+                break
+            case 3:
+                totalHours += 2
+                break
+            case 4:
+                totalHours += 2
+                break
+            case 5:
+                totalHours += 2
+                break
+            case 6:
+                totalHours += 0
+                break
+        }
+    }
+    return totalHours
 }
+
+console.log(countHours(year, holidays))
