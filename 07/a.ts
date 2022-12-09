1d0d80169528c9bd4511d9b1a97760dff191adce
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-12-09 02:40:57 -0600

    added solution of day 07

--- 07/a.ts
+++ 07/a.ts
@@ -1,11 +1,22 @@
-function createCube(size: number) {
-    const head = []
-    const tail = []
-    for (let i = 1; i <= size; i++) {
-        head.push(" ".repeat(size - i) + "/\\".repeat(i) + "_\\".repeat(size))
-        tail.push(" ".repeat(i - 1) + "\\/".repeat(size - (i - 1)) + "_/".repeat(size))
-    }
-    return [...head, ...tail].join("\n")
+const a1 = ['bike', 'car', 'bike', 'bike']
+const a2 = ['car', 'bike', 'doll', 'car']
+const a3 = ['bike', 'pc', 'pc']
+
+
+function getGiftsToRefill(a1: string[], a2: string[], a3: string[]) {
+
+    const a1Set = [...new Set(a1)]
+    const a2Set = [...new Set(a2)]
+    const a3Set = [...new Set(a3)]
+    const allSets = [...a1Set, ...a2Set, ...a3Set]
+
+    const map = new Map()
+    allSets.forEach((gift) => map.set(gift, map.has(gift) ? map.get(gift) + 1 : 1));
+    const uniqueVals = allSets.filter((gift) => map.get(gift) === 1)
+
+    return uniqueVals
+
 }
 
-console.log(createCube(6))
+getGiftsToRefill(a1, a2, a3)
+
