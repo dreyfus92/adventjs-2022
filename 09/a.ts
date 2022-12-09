03b50149768e8492d368ea84dbfaa22f0ff42b95
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-12-09 13:17:55 -0600

    added solution for day 09

--- 09/a.ts
+++ 09/a.ts
@@ -1,22 +1,9 @@
-const leds = [0, 0, 0, 1]
-
-// Every 7 seconds, the leds change state in this way:
-
-// If the led is off, it turns on if the led on its left (index - 1) was on before.
-// If the led is on, it remains on.
+const leds = [0, 1, 1, 0, 1]
 
 function countTime(leds: number[]) {
-    let time = 0
-    for (let i = 0; i < leds.length; i++) {
-        if (i === 0 && leds[i] === 0) {
-            leds[i] = 1
-            time += 7
-        } else if (leds[i] === 0 && leds[i - 1] === 1 && leds[i + 1] === 0) {
-            leds[i] = 1
-            time += 7
-        }
-    }
-    return time
+    const arrayofZeros = leds.join('').split('1')
+    arrayofZeros[0] += arrayofZeros.pop()
+    return arrayofZeros.sort((a, b) => b.length - a.length)[0].length * 7
 }
 
 console.log(countTime(leds))
