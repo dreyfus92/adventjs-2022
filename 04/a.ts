af001393b13496f32b553b6e97e35b52df5dde51
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-12-09 00:23:35 -0600

    day 04 solution

--- 04/a.ts
+++ 04/a.ts
@@ -4,13 +4,19 @@ type Box = {
     h: number
 }
 
-
 function fitsInOneBox(boxes: Box[]) {
-    // your code here
+    return boxes.sort((a, b) => {
+        return (a.l + a.w + a.h) - (b.l + b.w + b.h)
+    }).every((box, i) => {
+        if (i === 0) return true;
+        const prev = boxes[i - 1]
+        return box.l > prev.l && box.w > prev.w && box.h > prev.h
+    })
 }
 
-fitsInOneBox([
-    { l: 1, w: 1, h: 1 },
-    { l: 2, w: 2, h: 2 }
-]) // true
+console.log(fitsInOneBox([
+    { l: 1, w: 1, h: 10 },
+    { l: 3, w: 3, h: 12 },
+    { l: 2, w: 2, h: 1 },
+]))
 
