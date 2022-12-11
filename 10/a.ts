46be08148c7b64fb984c1a5721d252b7e924648c
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-12-10 19:18:24 -0600

    fixed typo

--- 10/a.ts
+++ 10/a.ts
@@ -1,9 +1,9 @@
 const heights = [1, 1000, 900, 800]
 
 function checkJump(heights: number[]) {
-    const inflection = heights.indexOf(Math.max(...heights));
-    const left = heights.slice(0, inflection + 1);
-    const right = heights.slice(inflection + 1);
+    const maxJump = heights.indexOf(Math.max(...heights));
+    const left = heights.slice(0, maxJump + 1);
+    const right = heights.slice(maxJump + 1);
     return (
         !(left.length <= 1 || !right.length) &&
         left.slice(1).every((number, index) => {
